9ce4b6316afd46dd91ac6209ad5a9f58057b3c84
priyanka24-cap <71220352+priyanka24-cap@users.noreply.github.com>
2020-09-19 14:23:18 +0530

    created list.py
    This file prints the positive numbers in a range.

---
+++ list.py
@@ -0,0 +1,11 @@
+list1= [12,-7,5,64,-11]
+for num in list1:
+    if num >=0:
+        print(num,end = " ")
+        
+#list2
+print("\n list2:")
+list2=[12,14,-95,3]
+for num1 in list2:
+    if num1>=0:
+        print(num1,end = " ")
